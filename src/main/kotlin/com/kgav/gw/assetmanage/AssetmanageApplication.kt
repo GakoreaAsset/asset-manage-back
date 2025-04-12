a1a238ca141193606ad5fae53295f480b5cf304f
Mingi Kang <gi3592@gmail.com>
2025-04-12 11:12:43 +0900

    #1-2. DB_connect

---
+++ src/main/kotlin/com/kgav/gw/assetmanage/AssetmanageApplication.kt
@@ -0,0 +1,11 @@
+package com.kgav.gw.assetmanage
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AssetmanageApplication
+
+fun main(args: Array<String>) {
+    runApplication<AssetmanageApplication>(*args)
+}
---
+++ src/test/kotlin/com/kgav/gw/assetmanage/AssetmanageApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kgav.gw.assetmanage
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssetmanageApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
