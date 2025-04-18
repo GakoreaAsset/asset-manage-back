914f9c39954c0000cf2c6750b1927c43b730398e
Mingi Kang <gi3592@gmail.com>
2025-04-18 17:01:25 +0900

    #2-1 db connect

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,15 +19,25 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    // mybatis와 JPA 구성
     runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")     // jpa
+    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:3.0.2") // mybatis
+
+    // 코틀린 기본 의존성
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin") //JSON 변환 모듈
+    implementation(kotlin("stdlib-jdk8")) // 코틀린 표준 라이브러리
+
+    // 스프링 코틀린 필수 의존
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 }
 
 kotlin {
